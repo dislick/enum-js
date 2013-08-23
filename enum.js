a13614c35bd638c99021bd503ef202fdca933f9d
Patrick Muff <muff.pa@gmail.com>
2013-08-23 17:58:34 +0200

    added main functionality

--- enum.js
+++ enum.js
@@ -1,3 +1,14 @@
 var Enum = function(enums) {
   this.enums = enums;
+  this.createEnumerables();
+}
+
+Enum.prototype.createEnumerables = function() {
+  var root = this;
+
+  this.enums.forEach(function(enumName) {
+    this[enumName] = {};
+  });
+
+  Object.freeze(this);
 }
