75530bde8ac2e5e8621cbb8b1cd8d5acfbf8f544
Patrick Muff <muff.pa@gmail.com>
2013-08-23 18:07:27 +0200

    added comments and whitespace

--- enum.js
+++ enum.js
@@ -4,11 +4,18 @@ var Enum = function(enums) {
 }
 
 Enum.prototype.createEnumerables = function() {
+  // get reference to this object
+  // for later use in .forEach loop
   var root = this;
 
+  // loop the input array and
+  // create a new property for each enum
+  // in the enum array. the empty object makes
+  // each property individual.
   this.enums.forEach(function(enumName) {
     root[enumName] = {};
   });
 
+  // freeze the object to ensure type safety
   Object.freeze(this);
 }
